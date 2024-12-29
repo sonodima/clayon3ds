48bdedada0c2d2c2dc6b64d2325dc09121ddcf6e
Tom <sonodimaa@icloud.com>
2024-12-30 00:40:13 +0100

    update onInteraction user param name

--- Source/Components/Include.h
+++ Source/Components/Include.h
@@ -8,7 +8,7 @@
 #include <clay.h>
 
 typedef Clay_String(*ListView_NameFetcher)(u32 index);
-typedef void(*ListView_OnInteraction)(Clay_ElementId, Clay_PointerData pointer, intptr_t /* u32 */ index);
+typedef void(*ListView_OnInteraction)(Clay_ElementId, Clay_PointerData pointer, intptr_t /* u32 */ data);
 
 void ListView(ListView_NameFetcher nameFetcher, ListView_OnInteraction onInteraction, u32 numItems, u32 curItem);
 void StatusBar(void);
